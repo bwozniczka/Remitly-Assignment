78fad4275df7e3d3613d031b0b972426741c355c
Bartłomiej Woźniczka <126593198+bwozniczka@users.noreply.github.com>
2024-04-22 00:01:45 +0200

    Delete Remilty directory

--- Remilty/main.py
+++
@@ -1,8 +0,0 @@
-import json
-import sys
-from json_validator import JsonValidator
-
-if len(sys.argv) >= 2:
-    print(JsonValidator(sys.argv[1]))
-else:
-    raise Exception("No json file name provided")
